f94295f0e435859042587db43fb0fd7c4ee2b499
Nick <Nick@Nick>
2016-05-16 01:02:22 +0000

    Pipezh ne poluchaetsa

--- etc/hello.py
+++
@@ -1,8 +0,0 @@
-from cgi import parse_qs
-
-
-def app(environ, start_response):
-        start_response('200 OK', [('Content-Type','text/plain')])
-        qs = qs.replace('&','\n')
-        return qs
-
--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
 def app(environ, start_response):
         start_response('200 OK', [('Content-Type','text/plain')])
-        qs = qs.replace('&','\n')
-        return qs
+        qs = environ ['QUERY_STRING'].split('&')
+        qs= [item + "\r\n" for item in qs]
+	return qs
 
