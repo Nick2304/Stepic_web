18e6f0f751a5a2f505aa1d56f7d13aa19bd0ac60
Nick2304 <nick2304@ukr.net>
2016-05-16 12:30:28 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 def app(environ, start_response):
-        start_response('200 OK', [('Content-Type','text/plain')])
-        qs = environ ['QUERY_STRING'].split('&')
-        qs= [item + "\r\n" for item in qs]
-	return qs
+     start_response('200 OK', [('Content-Type','text/plain')])
+     qs = environ['QUERY_STRING']
+     qs = qs.replace('&','\n\r')
+     return [qs]
 
